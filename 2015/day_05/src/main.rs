953bbee1dde4f63c917f9a13e9bd506c3ef2a2d2
Anthony Gedeon <anthony.gedeonn@gmail.com>
2023-05-26 22:42:17 -0500

    solve day 5 with cursed code

--- 2015/day_05/src/main.rs
+++ 2015/day_05/src/main.rs
@@ -1,10 +1,35 @@
-fn main() {
-    println!("{}", part_1("www"));
-    println!("{}", part_2("www"));
+use std::fs::File;
+use std::io::*;
+
+fn main() -> Result<()> {
+    let mut file = File::open("input.txt")?;
+    let mut input = String::new();
+    file.read_to_string(&mut input)?;
+
+    println!("{input}");
+
+    let mut nice_strings = 0;
+    for word in input.split_terminator("\n") {
+        if part_1(word) {
+            nice_strings += 1;
+        }
+    }
+    println!("{nice_strings}");
+
+    let mut nice_strings = 0;
+    for word in input.split_terminator("\n") {
+        if part_2(word) {
+            nice_strings += 1;
+        }
+    }
+    println!("{nice_strings}");
+
+    Ok(())
 }
 fn part_1(word: &str) -> bool {
     let word = word.to_lowercase();
     let mut nice_word = false;
+
     if word
         .chars()
         .filter(|&c| c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
@@ -15,12 +40,9 @@ fn part_1(word: &str) -> bool {
         nice_word = true;
     }
 
-    println!(
-        "{:?}",
-        word.chars()
-            .filter(|&c| c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
-            .collect::<Vec<char>>()
-    );
+    if nice_word == false {
+        return false;
+    }
 
     for pair in word.as_bytes().windows(2) {
         if pair.len() == 2 {
@@ -35,10 +57,13 @@ fn part_1(word: &str) -> bool {
 
     for pair in word.as_bytes().windows(2) {
         if pair.len() == 2 {
-            let w = format!("{:?}", pair);
-            if w != "ab" || w != "cd" || w != "pq" || w != "xy" {
-                nice_word = true;
-            } else {
+            let w = pair
+                .to_vec()
+                .iter()
+                .map(|&c| char::from(c))
+                .collect::<String>();
+            // println!("{}", w);
+            if w == "ab" || w == "cd" || w == "pq" || w == "xy" {
                 nice_word = false;
             }
         }
@@ -48,7 +73,38 @@ fn part_1(word: &str) -> bool {
 }
 
 fn part_2(word: &str) -> bool {
-    unimplemented!()
+    let mut result = false;
+
+    'a: for (i, pair_a) in word.as_bytes().windows(2).enumerate() {
+        for (j, pair_b) in word.as_bytes().windows(2).skip(2).enumerate() {
+            if j >= i {
+                if pair_a == pair_b {
+                    result = true;
+                    break 'a;
+                }
+            }
+        }
+    }
+
+    if result == false {
+        return false;
+    }
+
+    for i in 0..word.len() {
+        if (i + 2) >= word.len() {
+            break;
+        }
+        let letter_1 = word.chars().nth(i).unwrap();
+        let letter_2 = word.chars().nth(i + 2).unwrap();
+        if letter_1 == letter_2 {
+            result = true;
+            break;
+        } else {
+            result = false;
+        }
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -74,5 +130,25 @@ mod tests {
     #[test]
     fn test_naughty_word_3() {
         assert_eq!(part_1("dvszwmarrgswjxmb"), false)
+    }
+
+    #[test]
+    fn test_better_model_1() {
+        assert_eq!(part_2("qjhvhtzxzqqjkmpb"), true);
+    }
+
+    #[test]
+    fn test_better_model_2() {
+        assert_eq!(part_2("xxyxx"), true);
+    }
+
+    #[test]
+    fn test_better_model_3() {
+        assert_eq!(part_2("uurcxstgmygtbstg"), false);
+    }
+
+    #[test]
+    fn test_better_model_4() {
+        assert_eq!(part_2("ieodomkazucvgmuy"), false);
     }
 }

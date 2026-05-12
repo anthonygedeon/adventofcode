d9d4038d2d51e29ac77d3bd076376d2aba273bc4
anthonygedeon <anthony.gedeonn@gmail.com>
2026-05-12 11:24:35 -0500

    solve part 2

--- 2015/06/main.c
+++ 2015/06/main.c
@@ -5,6 +5,7 @@
 #include <errno.h>
 
 uint8_t grid[1000][1000] = {0};
+int grid2[1000][1000] = {0};
 
 void
 part_1(char *line)
@@ -46,10 +47,37 @@ part_1(char *line)
   }
 }
 
-int
+void
 part_2(char *line)
 {
-  return 0;
+  int total = 0;
+
+  char command[10] = {0}, state[4] = {0};
+  int x1, y1, x2, y2;
+
+  sscanf(line, "%s", command);
+
+  if (strncmp(line, "turn", 4) == 0)
+  {
+    sscanf(line, "%s %s %d,%d through %d,%d", command, state, &x1, &y1, &x2, &y2);
+
+    for (int row = x1; row <= x2; row++)
+	for (int col = y1; col <= y2; col++)
+	  if (strncmp(state, "off", 3) == 0)
+	    {
+	      if (grid2[row][col] > 0)
+		grid2[row][col]--;
+	    }
+	  else
+	    grid2[row][col]++;
+  }
+  else
+  {
+    sscanf(line, "%s %d,%d through %d,%d", command, &x1, &y1, &x2, &y2);
+    for (int row = x1; row <= x2; row++)
+	for (int col = y1; col <= y2; col++)
+	  grid2[row][col] += 2;
+    }
 }
 
 int
@@ -72,7 +100,12 @@ main(void)
 
   for (int i = 0; i < 1000; i++)
     for (int j = 0; j < 1000; j++)
-      if (grid[i][j]) total1++;
+      if (grid[i][j]) total1++; 
+
+  for (int i = 0; i < 1000; i++)
+    for (int j = 0; j < 1000; j++)
+      total2 += grid2[i][j];
+  
 
   printf("Part 1:%d\nPart 2:%d\n", total1, total2);
 
